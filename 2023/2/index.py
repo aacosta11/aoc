03f241e254dd3dff65e25726f752651a34046d2e
adrian acosta <acostaadrian2020@gmail.com>
2023-12-01 23:32:43 -0800

    2023 day 2 (updated pt.2)

--- 2023/2/index.py
+++ 2023/2/index.py
@@ -44,18 +44,16 @@ def one():
 # power = numbers of red, green, and blue cubes multiplied together
 
 def two():
+   getFirstAlphaCharIdx = lambda s, i: getFirstAlphaCharIdx(s,i + 1) if s[i].isdigit() else i
    sum = 0
    for game in input:
-      game = str(game.encode()).removeprefix('b\'').removesuffix('\\n\'').split(":")[1].replace(" ", "").split(";")
+      game = str(game.encode()).removeprefix('b\'').removesuffix('\\n\'').split(":")[1].replace(" ", "").replace(";", ",").split(",")
       leastNeeded = { "blue": 0, "green": 0, "red": 0 }
-      for set in game:
-         cubes = set.split(",")
-         for cube in cubes:
-            i = 0
-            while cube[i].isdigit(): i += 1
-            color = cube[i:]
-            val = int(cube[:i])
-            leastNeeded[color] = max(val, leastNeeded[color])
+      for cube in game:
+         i = getFirstAlphaCharIdx(cube, 0)
+         color = cube[i:]
+         val = int(cube[:i])
+         leastNeeded[color] = max(val, leastNeeded[color])
 
       power = leastNeeded["blue"] * leastNeeded["green"] * leastNeeded["red"]
       sum += power
